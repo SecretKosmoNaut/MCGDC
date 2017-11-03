fbcfbedba66f0803bfc4b63b1e5402e94e394751
Alexander Sosa <alexander.sosa1993@gmail.com>
2017-11-03 13:02:35 -0700

    Integration of new player animation attributes and slight restructuring.

--- megaman/animation.py
+++ megaman/animation.py
@@ -4,8 +4,14 @@ def animate(player):
         walking_frames = 4
         current_frame = player.timer.elapsed_time
 
+        if not player.moving_right and not player.moving_left:
+            player.image = pygame.image.load('images/megaman/stand.png')
+
         if player.moving_right:
             player.timer.update()
+
+            if player.facing_left:
+                player.facing_left = False
 
             if current_frame > 0.6:
                 player.image = pygame.image.load('images/megaman/walk4.png')
@@ -19,6 +25,9 @@ def animate(player):
         if player.moving_left:
             player.timer.update()
 
+            if not player.facing_left:
+                player.facing_left = True
+
             if current_frame > 0.6:
                 player.image = pygame.image.load('images/megaman/walk4.png')
             elif current_frame > 0.4:
@@ -28,7 +37,7 @@ def animate(player):
             else:
                 player.image = pygame.image.load('images/megaman/walk1.png')
 
+        if player.facing_left:
             player.image = pygame.transform.flip(player.image, True, False)
-
-        if not player.moving_right and not player.moving_left:
-            player.image = pygame.image.load('images/megaman/stand.png')
+        else:
+            player.image = pygame.transform.flip(player.image, False, False)
